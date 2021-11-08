3b1c14184c5c344919a98dfaf68934f36b708208
FUJITA Tomonori <fujita.tomonori@gmail.com>
2021-11-08 12:05:34 +0000

    bashreadline: use rlimit crate
    remove nix and libc dependency.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- examples/bashreadline/src/main.rs
+++ examples/bashreadline/src/main.rs
@@ -24,17 +24,6 @@ use tokio::io::AsyncReadExt;
 mod bashreadline;
 use bashreadline::*;
 
-fn bump_memlock_rlimit() {
-    let rlimit = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-
-    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
-        panic!("Failed to increase rlimit");
-    }
-}
-
 const BINARY_NAME: &str = "/bin/bash";
 const SYM_NAME: &str = "readline";
 const RINGBUF_NAME: &str = "rb";
@@ -61,7 +50,12 @@ impl Entry {
 
 #[tokio::main]
 async fn main() {
-    bump_memlock_rlimit();
+    rlimit::setrlimit(
+        rlimit::Resource::MEMLOCK,
+        rlimit::INFINITY,
+        rlimit::INFINITY,
+    )
+    .unwrap();
 
     let mut builder = BashreadlineSkelBuilder::default();
     builder.obj_builder.debug(true);
