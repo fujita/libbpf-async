052f78c9e05ed6863534efe0508c98791573cc37
FUJITA Tomonori <fujita.tomonori@gmail.com>
2023-06-29 22:37:08 +0900

    Revert "Do not steal libbpf's file descriptors"
    This reverts commit 6a0cf627069d1905b0b6324de9095f975270f3c8.
    
    mmap() internally holds the file object; we own the file. It's safe to
    use the file descriptor even if the Map object is dropped.
    
    So we can revert the commit.

--- libbpf-async/src/ringbuf.rs
+++ libbpf-async/src/ringbuf.rs
@@ -6,7 +6,7 @@ use core::task::{Context, Poll};
 use libbpf_rs::query::MapInfoIter;
 use std::io::Result;
 use std::num::NonZeroUsize;
-use std::os::unix::io::{AsRawFd, BorrowedFd, OwnedFd};
+use std::os::unix::io::RawFd;
 use tokio::io::unix::AsyncFd;
 use tokio::io::{AsyncRead, ReadBuf};
 
@@ -16,7 +16,7 @@ const BPF_RINGBUF_HDR_SZ: u32 = 8;
 
 pub struct RingBuffer {
     mask: u64,
-    async_fd: AsyncFd<OwnedFd>,
+    async_fd: AsyncFd<RawFd>,
     consumer: *mut core::ffi::c_void,
     producer: *mut core::ffi::c_void,
     data: *mut core::ffi::c_void,
@@ -24,25 +24,20 @@ pub struct RingBuffer {
 
 impl RingBuffer {
     pub fn new(map: &libbpf_rs::Map) -> Self {
-        // Borrowing this FD is safe: libbpf-rs returns only valid FDs and this borrow won't outlive the map reference
-        let original_map_fd = unsafe { BorrowedFd::borrow_raw(map.fd()) };
-        let map_fd = original_map_fd
-            .try_clone_to_owned()
-            .expect("Failed to duplicate FD");
         let mut max_entries = 0;
         for m in MapInfoIter::default() {
             if m.name == map.name() {
                 max_entries = m.max_entries;
             }
         }
-        let psize = NonZeroUsize::new(page_size::get()).expect("page size must not be zero");
+        let psize = page_size::get();
         let consumer = unsafe {
             nix::sys::mman::mmap(
                 None,
-                psize,
+                NonZeroUsize::new(psize).expect("page size must not be zero"),
                 nix::sys::mman::ProtFlags::PROT_WRITE | nix::sys::mman::ProtFlags::PROT_READ,
                 nix::sys::mman::MapFlags::MAP_SHARED,
-                map_fd.as_raw_fd(),
+                map.fd(),
                 0,
             )
             .unwrap()
@@ -50,22 +45,22 @@ impl RingBuffer {
         let producer = unsafe {
             nix::sys::mman::mmap(
                 None,
-                psize.saturating_add((2 * max_entries) as usize),
+                NonZeroUsize::new(psize + 2 * max_entries as usize)
+                    .expect("page size + 2 * max_entries must not be zero"),
                 nix::sys::mman::ProtFlags::PROT_READ,
                 nix::sys::mman::MapFlags::MAP_SHARED,
-                map_fd.as_raw_fd(),
-                psize.get() as i64,
+                map.fd(),
+                psize as i64,
             )
             .unwrap()
         };
 
-        let async_fd = AsyncFd::with_interest(map_fd, tokio::io::Interest::READABLE).unwrap();
         RingBuffer {
             mask: (max_entries - 1) as u64,
-            async_fd,
+            async_fd: AsyncFd::with_interest(map.fd(), tokio::io::Interest::READABLE).unwrap(),
             consumer,
             producer,
-            data: unsafe { producer.add(psize.get()) },
+            data: unsafe { producer.add(psize) },
         }
     }
 
